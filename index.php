32fe18e2506e4b82968ab26d2e5e2ab8704e176b
simplon <rudy__10@hotmail.fr>
2017-01-25 11:41:47 +0100

    rajout boucle foreach pour trouver le nombre de mot qui termine par la lettre q

--- index.php
+++ index.php
@@ -5,28 +5,67 @@
 	<title>Document</title>
 </head>
 <body>
-<?php 
-$string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
-$dico = explode("\n", $string);
+	<?php 
+	$string = file_get_contents("dictionnaire.txt", FILE_USE_INCLUDE_PATH);
+	$dico = explode("\n", $string);
 
-$nmbremot =count($dico);
-echo $nmbremot; ?></br>
+	$nmbremot =count($dico);
+	echo $nmbremot; ?></br>
 
-<?php  
-function lenghtCarct($dico){
+	<?php  
+	function lenghtCarct($dico){
 		$val=0;
-	foreach ($dico as $key => $value ) {
-      if(strlen($value) === 15){
-      	$val++;
-      }
-     } 
-     echo strval($val);
+		foreach ($dico as $key => $value ) {
+			if(strlen($value) === 15){
+				$val++;
+			}
+		} 
+		echo strval($val)."</br>";
 	}
-		lenghtCarct($dico);
-
- ?>
+	lenghtCarct($dico);
 
 
-	
+	function trouver($dico){
+		$val=0;
+		foreach ($dico as $key => $value ) {
+			if(strpos($value,"w") == true){
+				$val++;
+			}
+
+		}
+		echo strval($val)."</br>";
+	}
+	trouver($dico);
+
+// Affiche nombre de mot finissant par "q"
+
+
+	$num_words_q=0;
+foreach ($dico as $word) {
+	$last_letter = substr($word, -1);
+	if ($last_letter === "q") {
+		$num_words_q++;
+	}
+}
+echo  $num_words_q;
+
+
+
+
+
+?>
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
 </body>
 </html>
